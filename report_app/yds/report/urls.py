d67b1cab10bb78c6357aa17242150c5e9bba23ab
Tas-sos <tasosnumberten@yahoo.gr>
2017-12-25 23:07:32 +0200

    Adding the last url pattern for PDF production.

--- report_app/yds/report/urls.py
+++ report_app/yds/report/urls.py
@@ -12,6 +12,9 @@ urlpatterns = [
         # ex : /Attiki/
         path('<str:a_region>/', views.selected_region, name='selected_region'),
 
-        # ex : /Attiki/N. ANATOLIKIS ATTIKIS"
+        # ex : /Attiki/N. ANATOLIKIS ATTIKIS
         path('<str:a_region>/<str:a_municipality>/', views.selected_municipality, name='selected_municipality'),
+
+        # ex: /Attiki/N. ANATOLIKIS ATTIKI/383752
+        path('<str:a_region>/<str:a_municipality>/<int:project_url_id>/', views.create_pdf, name='create_pdf'),
     ]
