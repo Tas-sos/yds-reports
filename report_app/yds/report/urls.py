f4629b9f8aecaac081074275c9afeb9b983c66fc
Tas-sos <tasosnumberten@yahoo.gr>
2018-02-23 01:20:18 +0200

    Adding a error URL path.

--- report_app/yds/report/urls.py
+++ report_app/yds/report/urls.py
@@ -16,5 +16,15 @@ urlpatterns = [
         path('<str:a_region>/<str:a_municipality>/', views.selected_municipality, name='selected_municipality'),
 
         # ex: /Attiki/N. ANATOLIKIS ATTIKI/383752
-        path('<str:a_region>/<str:a_municipality>/<int:project_url_id>/', views.create_pdf, name='create_pdf'),
+        # path('<str:a_region>/<str:a_municipality>/<int:project_url_id>/', views.create_pdf, name='create_pdf'),
+
+        # ex : /static/report/download/390273.pdf
+        # path('static/report/download/<int:project_url_id>.pdf', views.create_pdf, name='create_pdf'),
+
+        # ex : /pdf_error/Attiki/N. ANATOLIKIS ATTIKI/389383
+        path('pdf_error/<str:a_region>/<str:a_municipality>/<int:project_url_id>/', views.pdf_error, name='pdf_error'),
+
+        # ex : /ajax/create_pdf/389383
+        path('ajax/create_pdf/<int:project_url_id>', views.create_pdf, name='create_pdf' ),
+
     ]
