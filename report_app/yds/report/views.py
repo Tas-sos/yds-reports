278524b2faaa81ddba32fa1dcd540ff309728197
Tas-sos <tasosnumberten@yahoo.gr>
2018-02-23 01:25:25 +0200

    Bugs fixing & adding of "pdf_error" and "delete_PDFs" views.

--- report_app/yds/report/views.py
+++ report_app/yds/report/views.py
@@ -5,7 +5,11 @@ from django.core.files.storage import FileSystemStorage
 from utilities.data_set import regions, municipality
 from utilities.project import get_projects_by_municipality, get_project_data, search, generate_pdf, pdf_name
 
-from os.path import dirname, abspath, join  # To define the "STATIC_ROOT" path. ( Look at the "yds/settings.py" file. )
+from os.path import dirname, abspath, join, getctime
+from os import listdir, remove
+from datetime import datetime, timedelta
+import time
+import threading
 
 
 
@@ -81,6 +85,10 @@ def selected_municipality(request, a_region, a_municipality):
 
                     projects = get_projects_by_municipality(a_municipality)
 
+                    # Διόρθωση του project url ώστε να παίρνω μονάχα το id του.
+                    for name, url in projects.items():
+                        projects[name] = url.split('/')[5]
+
                     context = {'selected_region': a_region,
                                'selected_municipality': a_municipality,
                                'projects': projects,
@@ -95,25 +103,24 @@ def selected_municipality(request, a_region, a_municipality):
 
 
 
-def create_pdf(request, a_region, a_municipality, project_url_id):
+def create_pdf(request, project_url_id):
     """
-    Δημιουργία του εγγράφου αναφοράς PDF και επιστροφή αυτού στον χρήστη.
+    Δημιουργία του εγγράφου αναφοράς.
 
     Η παρούσα συνάρτηση είναι υπεύθυνη ώστε να προσπαθήσει να δημιουργήσει ένα PDF έγγραφο αναφοράς για το έργο που θα
-    της ζητηθεί και έπειτα να το επιστρέψει στον χρήστη ώστε να το κατεβάσει.
-
-    TODO: Να ανοίγει μια νέα καρτέλα μονάχα όταν υπάρχει πρόβλημα στην παραγωγή του PDF.
+    της ζητηθεί και έπειτα να το επιστρέψει ένα μήνυμα επιτυχής παραγωγής του ή όχι.
+    Αυτή η συνάρτηση καλείται μέσω AJAX.
 
     :param request: Το αίτημα του χρήστη.
-    :param a_region: Μια περιφερειακή ενότητα.
-    :param a_municipality: Ένας νομός της παραπάνω περιφερειακής ενότητας.
-    :param project_url_id: Το id του έργου ( όπως είναι στο «linkedeconomy.org» ).
-    :return: Ένα έγγραφο αναφοράς σε μορφή PDF με τα στοιχεία του έργου που δόθηκε ως παράμετρος.
+    :param project_url_id: Το id του έργου ( όπως είναι στο «linkedeconomy.org» αλλά μονάχα το id του ).
+    :return: Είτε το έγγραφο αναφοράς σε μορφή PDF με τα στοιχεία του έργου που δόθηκε ως παράμετρος, είτε "status=504"
+             το οποίο σημαίνει πως για κάποιο λόγο δεν ήταν επιτυχής η παραγωγή του PDF.
     """
 
     url = "http://linkedeconomy.org/resource/PublicWork/" + str(project_url_id)
     file_name = str(project_url_id) + ".pdf"
 
+    # To define the "STATIC_ROOT" path. ( Look at the "yds/settings.py" file. )
     base_dir = dirname( dirname(abspath(__file__)) )
     static_root = join(base_dir, "static/report/download/")
 
@@ -121,7 +128,15 @@ def create_pdf(request, a_region, a_municipality, project_url_id):
     project_data = get_project_data(url)
 
     # Search for related articles for this project :
-    related_articles = search(project_data['title'])
+    try:
+        related_articles = search(project_data['title'])
+    except:
+        related_articles = {'Δεν ήταν δυνατή η εύρεση σχετικών δημοσιεύσεων.': '#'}
+
+
+    # Delete olf PDFs:
+    threading.Thread(target=delete_PDFs, args=(static_root,), kwargs={}, daemon=True).start()
+
 
     # Generating the report PDF for this project :
     try:
@@ -134,10 +149,9 @@ def create_pdf(request, a_region, a_municipality, project_url_id):
         print(log)
         print('-------------------------------------------------------------------------------------------------------')
 
-        return render(request, 'report/pdf.html', {'project_url': url})
+        return HttpResponse(status=504)
 
-    # Create response :
-
+    # Create success response :
     file_system = FileSystemStorage(static_root)
     with file_system.open(file_name) as pdf:
 
@@ -146,3 +160,55 @@ def create_pdf(request, a_region, a_municipality, project_url_id):
         response['Content-Disposition'] = 'attachment; filename="' + file_name + '"'
 
         return response
+
+
+
+
+def pdf_error(request, a_region, a_municipality, project_url_id):
+    """
+    Εμφάνιση μηνύματος σφάλματος για την παραγωγή ενός PDF.
+
+    Όταν καλείται αυτή η συνάρτηση, τότε σημαίνει πως η παραγωγή ενός PDF δεν ήταν επιτυχής και αυτό το view αναλαμβάνει
+    μονάχα να εμφανίσει μια σελίδα σφάλματα προς ενημέρωση του χρήστη.
+
+    :param request: Το αίτημα του χρήστη.
+    :param a_region: Σε ποια περιφερειακή ενότητα ανήκει το έργο που δεν ήταν εφικτή η παραγωγή του PDF.
+    :param a_municipality: Σε ποιο νομό της παραπάνω περιφερειακής ενότητας ανήκει το έργο.
+    :param project_url_id: Για ποιο έργο δεν ήταν εφικτή η παραγωγή του PDF.
+    :return: Την σελίδα «report/pdf.html» με τις παραπάνω πληροφορίες.
+    """
+
+    context = {'region': a_region,
+               'municipality': a_municipality,
+               'project_id': project_url_id}
+
+    return render(request, 'report/pdf.html', context)
+
+
+
+
+def delete_PDFs( folder_path ):
+    """
+    Μέθοδος που αναλαμβάνει να διαγράψει όλα τα PDFs που πλέον θεωρούνται παλιά.
+
+    Είναι μια πάρα πολύ σημαντική μέθοδος η οποία αναλαμβάνει να διαγράψει τα παλιά PDFs.
+    Χωρίς αυτή ο αποθηκευτικός χώρος του διακομιστή κάποια στιγμή θα γέμιζε πλήρως και ο λόγος θα ήταν τα PDF.
+
+    Αυτή η μέθοδος τρέχει παράλληλα κάθε φορά που εκτελείται η μέθοδος παραγωγής ενός νέου εγγράφου PDF.
+
+    :param folder_path: Ο κατάλογος που αποθηκεύονται τα PDFs.
+    :return: Θα πρέπει να έχουν διαγραφεί τα έργα που είναι παραπάνω από μια ώρα δημιουργημένα.
+    """
+
+    # Find PDF files.
+    files = [ f for f in listdir(folder_path) ]
+
+    # The time from an hour ago
+    one_hour_before = datetime.now() + timedelta(hours=-1)
+
+    # Delete older files
+    for pdf in files:
+        if datetime.strptime(time.ctime(getctime( folder_path + pdf )), "%a %b %d %H:%M:%S %Y") < one_hour_before:
+            remove(folder_path + pdf)
+
+
